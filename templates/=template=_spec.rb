db22744bd8a35d140c1c95175d0356a880f5e3a6
alanqian <qianguoxiang2008@gmail.com>
2019-04-18 22:02:31 +0800

    add rspec template

---
+++ templates/=template=_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# %FFILE%
+# Copyright (C) %YEAR% %USER% <%MAIL%>
+#
+# Distributed under terms of the %LICENSE% license.
+#
+
+RSpec.describe Module::Class do
+  it 'has something here' do
+    expect(true).to be false
+    %HERE%
+  end
+end
